f639e73be1b2116e318f2485cdf9ea8b71c301e1
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-11-13 17:27:07 +0200

    test: Add unit tests of tee command

---
+++ ch04/src/tee/cmd/root_test.go
@@ -0,0 +1,252 @@
+package cmd
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
+)
+
+func createPipe(t *testing.T) (*os.File, *os.File, error) {
+	r, w, err := os.Pipe()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	t.Cleanup(func() {
+		r.Close()
+		w.Close()
+	})
+
+	return r, w, err
+}
+
+func TestWriteToStdout(t *testing.T) {
+	originalStdin := os.Stdin
+	originalStdout := os.Stdout
+
+	rStdout, wStdout, err := createPipe(t)
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	os.Stdout = wStdout
+
+	rStdin, wStdin, err := createPipe(t)
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	os.Stdin = rStdin
+
+	wStdin.Write([]byte("Hello, World!\n"))
+	wStdin.Close()
+
+	rootCmd.Execute()
+	wStdout.Close()
+
+	os.Stdout = originalStdout
+	os.Stdin = originalStdin
+
+	var buf bytes.Buffer
+	io.Copy(&buf, rStdout)
+
+	actual := buf.String()
+	expected := "Hello, World!\n"
+
+	if actual != expected {
+		t.Errorf("expected: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestWriteToFile(t *testing.T) {
+	originalStdin := os.Stdin
+
+	rStdin, wStdin, err := createPipe(t)
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	os.Stdin = rStdin
+
+	wStdin.Write([]byte("Hello, World!\n"))
+	wStdin.Close()
+
+	rootCmd.SetArgs([]string{"/tmp/test.txt"})
+	rootCmd.Execute()
+
+	os.Stdin = originalStdin
+
+	f, err := os.Open("/tmp/test.txt")
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	defer func() {
+		f.Close()
+		os.Remove("/tmp/test.txt")
+	}()
+
+	var buf2 bytes.Buffer
+	io.Copy(&buf2, f)
+
+	actual := buf2.String()
+	expected := "Hello, World!\n"
+
+	if actual != expected {
+		t.Errorf("expected: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestWriteToMultipleFiles(t *testing.T) {
+	originalStdin := os.Stdin
+
+	rStdin, wStdin, err := createPipe(t)
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	os.Stdin = rStdin
+
+	wStdin.Write([]byte("Hello, World!\n"))
+	wStdin.Close()
+
+	rootCmd.SetArgs([]string{"/tmp/test.txt", "/tmp/test2.txt"})
+	rootCmd.Execute()
+
+	os.Stdin = originalStdin
+
+	f, err := os.Open("/tmp/test.txt")
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	defer func() {
+		f.Close()
+		os.Remove("/tmp/test.txt")
+	}()
+
+	var buf bytes.Buffer
+	io.Copy(&buf, f)
+
+	actual := buf.String()
+	expected := "Hello, World!\n"
+
+	if actual != expected {
+		t.Errorf("expected: %v, actual: %v", expected, actual)
+	}
+
+	f2, err := os.Open("/tmp/test2.txt")
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	defer func() {
+		f2.Close()
+		os.Remove("/tmp/test2.txt")
+	}()
+
+	var buf2 bytes.Buffer
+	io.Copy(&buf2, f2)
+
+	actual2 := buf2.String()
+	expected2 := "Hello, World!\n"
+
+	if actual2 != expected2 {
+		t.Errorf("expected: %v, actual: %v", expected2, actual2)
+	}
+}
+
+func TestWriteToStdoutAndFile(t *testing.T) {
+	originalStdin := os.Stdin
+	originalStdout := os.Stdout
+
+	rStdout, wStdout, err := createPipe(t)
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	os.Stdout = wStdout
+
+	rStdin, wStdin, err := createPipe(t)
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	os.Stdin = rStdin
+
+	wStdin.Write([]byte("Hello, World!\n"))
+	wStdin.Close()
+
+	rootCmd.SetArgs([]string{"/tmp/test.txt"})
+	rootCmd.Execute()
+	wStdout.Close()
+
+	os.Stdout = originalStdout
+	os.Stdin = originalStdin
+
+	f, err := os.Open("/tmp/test.txt")
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	defer func() {
+		f.Close()
+		os.Remove("/tmp/test.txt")
+	}()
+
+	var buf bytes.Buffer
+	io.Copy(&buf, f)
+
+	actual := buf.String()
+	expected := "Hello, World!\n"
+
+	if actual != expected {
+		t.Errorf("expected: %v, actual: %v", expected, actual)
+	}
+
+	var buf2 bytes.Buffer
+	io.Copy(&buf2, rStdout)
+
+	actual2 := buf2.String()
+	expected2 := "Hello, World!\n"
+
+	if actual2 != expected2 {
+		t.Errorf("expected: %v, actual: %v", expected2, actual2)
+	}
+}
+
+func TestAppendToFile(t *testing.T) {
+	f, err := os.Create("/tmp/test.txt")
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	n, err := f.Write([]byte("Hello\n"))
+	if len([]byte("Hello\n")) != n || err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	f.Close()
+
+	originalStdin := os.Stdin
+
+	rStdin, wStdin, err := createPipe(t)
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+	os.Stdin = rStdin
+
+	wStdin.Write([]byte("World\n"))
+	wStdin.Close()
+
+	rootCmd.SetArgs([]string{"/tmp/test.txt", "-a"})
+	rootCmd.Execute()
+
+	os.Stdin = originalStdin
+
+	f, err = os.Open("/tmp/test.txt")
+	if err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	var buf bytes.Buffer
+	io.Copy(&buf, f)
+
+	actual := buf.String()
+	expected := "Hello\nWorld\n"
+
+	if actual != expected {
+		t.Errorf("expected: %v, actual: %v", expected, actual)
+	}
+}
